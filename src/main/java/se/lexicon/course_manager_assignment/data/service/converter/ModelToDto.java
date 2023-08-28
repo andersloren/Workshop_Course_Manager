4c9ddc763f62b569430b62c9dd19ca7f735801c0
Liebniz <anders.leif.loren@liebniz.com>
2023-08-28 09:04:42 +0200

    All testing for ModelToDto is done

--- src/main/java/se/lexicon/course_manager_assignment/data/service/converter/ModelToDto.java
+++ src/main/java/se/lexicon/course_manager_assignment/data/service/converter/ModelToDto.java
@@ -1,35 +1,47 @@
 package se.lexicon.course_manager_assignment.data.service.converter;
 
 import org.springframework.stereotype.Component;
+import se.lexicon.course_manager_assignment.data.dao.StudentCollectionRepository;
+import se.lexicon.course_manager_assignment.data.sequencers.StudentSequencer;
 import se.lexicon.course_manager_assignment.dto.views.CourseView;
 import se.lexicon.course_manager_assignment.dto.views.StudentView;
 import se.lexicon.course_manager_assignment.model.Course;
 import se.lexicon.course_manager_assignment.model.Student;
 
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 @Component
 public class ModelToDto implements Converters {
     @Override
     public StudentView studentToStudentView(Student student) {
-        StudentView studentView = new StudentView(student.getId(), student.getName(), student.getEmail(), student.getAddress());
-        return studentView;
+        return new StudentView(student.getId(), student.getName(), student.getEmail(), student.getAddress());
     }
 
     @Override
     public CourseView courseToCourseView(Course course) {
-        return null;
+        return new CourseView(course.getId(), course.getCourseName(), course.getStartDate(), course.getWeekDuration(), studentsToStudentViews(course.getStudents()));
     }
 
     @Override
     public List<CourseView> coursesToCourseViews(Collection<Course> courses) {
-        return null;
+        List<CourseView> coursesView = new ArrayList<>();
+        for (Course course : courses) {
+            coursesView.add(courseToCourseView(course));
+        }
+        return coursesView;
     }
 
     @Override
     public List<StudentView> studentsToStudentViews(Collection<Student> students) {
-        return null;
+        List<StudentView> studentsView = new ArrayList<>();
+        for (Student student : students) {
+            studentsView.add(studentToStudentView(student));
+        }
+        return studentsView;
     }
 }
--- src/test/java/se/lexicon/course_manager_assignment/data/service/converter/ModelToDtoTest.java
+++ src/test/java/se/lexicon/course_manager_assignment/data/service/converter/ModelToDtoTest.java
@@ -4,14 +4,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import se.lexicon.course_manager_assignment.data.dao.CourseCollectionRepository;
 import se.lexicon.course_manager_assignment.data.dao.StudentCollectionRepository;
 import se.lexicon.course_manager_assignment.dto.views.StudentView;
+import se.lexicon.course_manager_assignment.model.Course;
 import se.lexicon.course_manager_assignment.model.Student;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = {ModelToDto.class})
 public class ModelToDtoTest {
@@ -20,15 +24,64 @@ public class ModelToDtoTest {
     private Converters testObject;
 
     @Test
-    @DisplayName("Should return a StudentView object from a Student object")
-    StudentView studentToStudentView() {
-        StudentCollectionRepository repo = new StudentCollectionRepository(new HashSet<Student>());
-        Student studentMikael = repo.createStudent("Mikael Engvall", "mikael@engvall.se", "Vallgatan 22");
+    @DisplayName("A StudentView object from a Student object should hold the same content")
+    void studentToStudentView() {
+        StudentCollectionRepository repo = new StudentCollectionRepository(new HashSet<>());
+        Student studentMikael = repo.createStudent("Mikael Engvall", "mikael@engvall.org", "Vallgatan 22");
 
-        StudentView studentView = new StudentView(studentMikael.getId(), studentMikael.getName(), studentMikael.getEmail(), studentMikael.getAddress());
+        ModelToDto modelToDto = new ModelToDto();
 
-        assertEquals(studentMikael.toString(), studentView.toString());
-    } // TODO: I don't understand this...
+        assertEquals(studentMikael.getName(), modelToDto.studentToStudentView(studentMikael).getName());
+    } // This one is working
+
+    @Test
+    @DisplayName("")
+    void courseToCourseView() { // This is working
+        StudentCollectionRepository studentRepo = new StudentCollectionRepository(new HashSet<>());
+        Student studentMikael = studentRepo.createStudent("Mikael Engvall", "mikael@engvall.org", "Vallgatan 22");
+        Student studentAnders = studentRepo.createStudent("Anders Loren", "anders@loren.org", "Jutevagen 22");
+
+        CourseCollectionRepository courseRepo = new CourseCollectionRepository(new HashSet<Course>());
+        Course courseJava = courseRepo.createCourse("Java", LocalDate.of(2023, 8, 25), 12);
+
+        courseJava.enrollStudent(studentMikael);
+        courseJava.enrollStudent(studentAnders);
+
+        ModelToDto modelToDto = new ModelToDto();
+        assertEquals(courseJava.getCourseName(), modelToDto.courseToCourseView(courseJava).getCourseName());
+
+        // Uncomment to see that: Java, Java
+//        System.out.println(courseJava.getCourseName());
+//        System.out.println(modelToDto.courseToCourseView(courseJava).getCourseName());
+    }
+
+    @Test
+    @DisplayName("")
+    void courseToCourseViews() {
+        CourseCollectionRepository coursesRepo = new CourseCollectionRepository(new HashSet<Course>());
+        Course courseJava = coursesRepo.createCourse("Java", LocalDate.of(2023, 8, 25), 12);
+
+        ModelToDto modelToDto = new ModelToDto();
+
+        assertEquals(courseJava.getCourseName(), modelToDto.courseToCourseView(courseJava).getCourseName());
+    } // This is working
+
+    @Test
+    @DisplayName("") // TODO: Give this test a description
+    void studentsToStudentViews() {
+        StudentCollectionRepository repo = new StudentCollectionRepository(new HashSet<>());
+        Student studentMikael = repo.createStudent("Mikael Engvall", "mikael@engvall.org", "Vallgatan 22");
+        Student studentAnders = repo.createStudent("Anders Loren", "anders@loren.org", "Jutevagen 22");
+
+        HashSet<Student> studentsHashSet = new HashSet<>();
+        studentsHashSet.add(studentAnders);
+        studentsHashSet.add(studentMikael);
+
+        ModelToDto modelToDto = new ModelToDto();
+        List<StudentView> studentsViewList = new ArrayList<>(modelToDto.studentsToStudentViews(studentsHashSet));
+        String name = studentsViewList.get(0).getName();
+        assertTrue(name.equalsIgnoreCase("Anders Loren") || name.equalsIgnoreCase("Mikael Engvall"));
+    } // This one is working
 
     @Test
     @DisplayName("Test context successfully setup")
